e5c47e5990fa6b3385261e087f1dea0cb01243fb
Mike Taves <mwtoews@gmail.com>
2022-11-10 12:12:16 +1300

    Create pyproject.toml for metadata and dependencies

--- setup.py
+++ setup.py
@@ -1,22 +1,4 @@
-import setuptools
-from geostatspy import __version__
+"""See pyproject.toml for project metadata."""
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="geostatspy",
-    version=__version__,
-    author="Michael Pyrcz",
-    author_email="mpyrcz@austin.utexas.edu",
-    description="Geostatistical methods from GSLIB: Geostatistical Library translated and reimplemented in Python",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/GeostatsGuy/GeostatsPy",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-)
+setup()
