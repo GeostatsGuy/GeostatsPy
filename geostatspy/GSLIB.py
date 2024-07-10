9a1be9dd1328885242c03c3485f639abaecec4d0
Michael Pyrcz <mpyrcz@austin.utexas.edu>
2024-07-10 15:58:09 -0500

    Update GSLIB.py

--- geostatspy/GSLIB.py
+++ geostatspy/GSLIB.py
@@ -871,8 +871,8 @@ def conditioning_check(model,nx,xmn,xsiz,ny,ymn,ysiz,nreal,df,xcol,ycol,vcol,vna
     
     iix = np.zeros(len(df),dtype = int); iiy = np.zeros(len(df),dtype = int)
     for idata in range(0,len(df)):                                # find the model ix, iy cells at data x, y
-        iix[idata] = max(0,min(geostats.getindex(nx,xmn,xsiz,df.loc[idata,xcol]),nx-1))
-        iiy[idata] = ny - max(0,min(geostats.getindex(ny,ymn,ysiz,df.loc[idata,ycol]),nx-1)) - 1
+        iix[idata] = max(0,min(int((df.loc[idata,xcol] - xmin)/xsiz),nx-1))
+        iiy[idata] = ny - max(0,min(int((df.loc[idata,ycol] - ymin)/ysiz),ny-1))-1
         
     real_por = np.zeros(len(df))
     all_data = np.zeros(len(df)*nreal)
@@ -892,7 +892,7 @@ def conditioning_check(model,nx,xmn,xsiz,ny,ymn,ysiz,nreal,df,xcol,ycol,vcol,vna
     plt.scatter(all_data,all_real,alpha=0.2,color='black',s=5)
     plt.plot([vmin,vmax],[vmin,vmax],color='black',linestyle='--')
     plt.ylabel('Simulated Realizations - ' + vname); plt.xlabel('Data Values - ' + vname); plt.title('Data Reproduction Check - ' + vname)
-    plt.grid(True); plt.xlim([vmin,vmax]); plt.ylim([vmin,vmax])
+    plt.xlim([vmin,vmax]); plt.ylim([vmin,vmax])
     return df_vstack
 
 def distribution_check(df,vcol,model,nx,ny,nreal,dreal,vname,vmin,vmax):
